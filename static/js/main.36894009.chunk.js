5d1dbac218f9a4db4a9a7f0dc18b186d43d3a128
@V.Prisyagnuk <v.prisyagnuk@esverito.com>
2021-11-13 12:04:20 +0200

    Updates

---
+++ static/js/main.36894009.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpStore=this.webpackJsonpStore||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);e(0);var c=e(3),o=e.n(c),i=(e(8),e(9),e(1));var r=function(){return Object(i.jsx)("div",{children:"Test"})},u=function(t){t&&t instanceof Function&&e.e(3).then(e.bind(null,12)).then((function(n){var e=n.getCLS,c=n.getFID,o=n.getFCP,i=n.getLCP,r=n.getTTFB;e(t),c(t),o(t),i(t),r(t)}))};o.a.render(Object(i.jsx)(r,{}),document.getElementById("root")),u()},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.36894009.chunk.js.map
